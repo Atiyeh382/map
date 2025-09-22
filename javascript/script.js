4b83b2479418d726094b30f5429e6fe23a35c69c
Atiyeh Tabatabaei <atiehtaba82@gmail.com>
2025-09-22 16:51:43 +0330

    add marker

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+L.marker([35.6892, 51.3890]).addTo(map)
+  .bindPopup("Tehran, Iran")
+  .openPopup();
+
